33796b89ff677bc2df70f5bb1bf49e83a06ed957
Jose Acena <joseracena@gmail.com>
2023-12-08 10:44:42 +0100

    added whitespace

--- DTM.py
+++ DTM.py
@@ -4,9 +4,9 @@
 def delay(): 
     print("Delaying...")
 
-print("Welcome to the Deterministic Turing Machine Simulator")
+print("\nWelcome to the Deterministic Turing Machine Simulator")
 delay()
 print("The machine is set by default to identify the languague a^n b^n c^n, n element No ")
 delay()
-word = input("Enter the word you wish to check")
-print("You wrote" + word)
+word = input("Enter the word you wish to check ")
+print("You wrote " + word)
