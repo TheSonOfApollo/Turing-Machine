ac189e73f6a4a54ffec871155d30691e919adc0b
Jose Acena <joseracena@gmail.com>
2023-12-08 10:04:10 +0100

    created basic program & structure

---
+++ DTM.py
@@ -0,0 +1,12 @@
+# write description here 
+# write some more 
+
+def delay(): 
+    print("Delaying...")
+
+print("Welcome to the Deterministic Turing Machine Simulator")
+delay()
+print("The machine is set by default to identify the languague a^n b^n c^n, n element No ")
+delay()
+word = input("Enter the word you wish to check")
+print("You wrote" + word)
