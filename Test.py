3586564fdda940b8daa9a5d815003f5ed222bca8
Jose Acena <joseracena@gmail.com>
2023-12-08 14:27:53 +0100

    added the delay function to the retro function

--- Test.py
+++ Test.py
@@ -14,11 +14,16 @@ def retro(text):
         print(char, end="")
         sys.stdout.flush()
         time.sleep(0.025) 
+    delayTime = 14000000
+    while delayTime > 0:
+        delayTime -= 1
+    print("\n")
+    
         
 
 
 
-# Definitions are kept up here ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Definitions are kept up here ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 print("\n")
 print("Hello World")
@@ -31,5 +36,17 @@ testNumber = 3
 delay_output("This is the third part of the test, tests until now: " + str(testNumber))
 '''
 
+'''
 print("\n") 
 retro("hello this is a retro test")
+print("\n") 
+delay_output(retro("Hello"))
+print("\n") 
+delay_output(retro("If you are seing this"))
+print("\n") 
+delay_output(retro("The test was succesful!"))
+'''
+
+print("\n") 
+retro("This is another test")
+retro("Now we are testing other things")
