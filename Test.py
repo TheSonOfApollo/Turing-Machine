d2beb1409bbce7c86ed489b71bb1cac3a6356d96
Jose Acena <joseracena@gmail.com>
2023-12-08 11:16:47 +0100

    created retro function

--- Test.py
+++ Test.py
@@ -10,9 +10,25 @@ def delay_output(text):
         i -= 1
     print(text)
 
+def retro(text): 
+    delay = 10000000
+    i = len(text)
+    k = 0
+    while k < i: 
+        print(text[k])
+        k += 1
+        while delay > 0: 
+            delay -= 1
+
+
+
+# Definitions are kept up here ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 print("\n")
 delay_output("This is a test")
 delay_output("This is the second part of the text")
 testNumber = 3
 delay_output("This is the third part of the test, tests until now: " + str(testNumber))
+
+print("\n") 
+retro("hello this is a retro test")
