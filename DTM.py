0a29e335604704c3951582ab401f768d70899e11
Jose Acena <joseracena@gmail.com>
2023-12-09 13:34:12 +0100

    adjusted retro speed

--- DTM.py
+++ DTM.py
@@ -2,15 +2,12 @@
 # write some more 
 
 import sys, time
-
-def delay(): 
-    print("Delaying...")
     
 def retro(text): 
     for char in text: 
         print(char, end="")
         sys.stdout.flush()
-        time.sleep(0.025) 
+        time.sleep(0.07) 
     delayTime = 14000000
     while delayTime > 0:
         delayTime -= 1
@@ -24,4 +21,7 @@ retro("Welcome to the Deterministic Turing Machine Simulator")
 retro("The machine is set by default to identify the languague a^n b^n c^n, n element No")
 word = input("Enter the word you wish to check ")
 retro("You wrote: " + word)
+retro("Right now the word you choose will be processed in real time")
+retro("Should your word be accepted, you will see all the letters have been replaced by 'x'")
+retro("Proccessing...")
 
